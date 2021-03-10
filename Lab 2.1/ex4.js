8696df8919c8e842319dd7155e5a9e7e520f8fe1
Bùi Quốc Trọng <buiquoctrong110500@gmail.com>
2021-03-10 21:15:55 +0700

    Update

---
+++ Lab 2.1/ex4.js
@@ -0,0 +1,45 @@
+var error = document.getElementById('error');
+var answer = document.getElementById('answer');
+var x1 = document.getElementById('x1');
+var x2 = document.getElementById('x2');
+var delta = document.getElementById('delta');
+
+function solveEquation() {
+    var a = document.getElementById("A").value;
+    var b = document.getElementById("B").value;
+    var c = document.getElementById("C").value;
+
+    if (a == 0) {
+        error.innerHTML = "A phải khác 0!";
+        document.getElementById("A").value = "";
+        document.getElementById("A").focus();
+        return;
+    } else {
+        var d = b*b - 4*a*c;
+        delta.innerHTML = d;
+        if (d > 0) {
+            answer.innerHTML = "Phương trình có 2 nghiệm phân biệt";
+            var root1 = (-b + Math.sqrt(d)) / (2*a);
+            var root2 = (-b - Math.sqrt(d)) / (2*a);
+            x1.innerHTML = root1;
+            x2.innerHTML = root2;
+            delta.innerHTML = d + ', > 0';
+        } else if (d === 0) {
+            answer.innerHTML = "Phương trình có nghiệm kép";
+            var root = -b/(2*a);
+            x1.innerHTML = root;
+            x2.innerHTML = root;
+        } else {
+            answer.innerHTML = "Phương trình vô nghiệm";
+            x1.innerHTML = 'NaN';
+            x2.innerHTML = 'NaN';
+        }
+    }
+}
+
+
+var submitButton = document.getElementById('submit');
+submitButton.onclick = function() {
+    solveEquation();
+};
+
---
+++ Lab 2.1/ex5.js
@@ -0,0 +1,116 @@
+function validateEmail(inputText) {
+    var mailformat = /\S+@\S+\.\S+/;
+    if(!inputText.value.match(mailformat)) {
+        document.getElementById('errorOfEmail').innerHTML = "You have entered a invalid email address!";
+    } else {
+        document.getElementById('errorOfEmail').innerHTML = "";
+    }
+}
+
+function validateZipCode(code) {
+    regexp = /^\d{5}$/;
+    if (!regexp.test(code.value)){
+        document.getElementById('errorOfZIPCode').innerHTML = "You have entered a invalid ZIP code!"; 
+    } else {
+        document.getElementById('errorOfZIPCode').innerHTML = "";
+    }
+}
+
+function validateSex() {
+    var male = document.getElementById('Male');
+    var female = document.getElementById("Female");
+
+    if(!male.checked && !female.checked) {
+        document.getElementById('errorOfSex').innerHTML = "You must select male or female";
+    } else {
+        document.getElementById('errorOfSex').innerHTML = "";
+    }
+}
+
+function checkEmpty() {
+    var inputs = document.getElementsByClassName("check")
+    for (var i = 0; i < inputs.length; i++) {
+        if (inputs[i].value == ""){
+            alert('Input can not be left blank');
+            return true;
+        }
+    }
+
+    var male = document.getElementById('Male');
+    var female = document.getElementById("Female");
+    if (!male.checked && !female.checked) {
+        alert('Input can not be left blank');
+        return true;
+    }
+    return false;
+}
+
+function validateDate(dateValue)
+{
+    var selectedDate = dateValue;
+    if(selectedDate == '')
+        return false;
+
+    var regExp = /^(\d{1,2})(\/|-)(\d{1,2})(\/|-)(\d{4})$/;
+    var dateArray = selectedDate.match(regExp);
+
+    if (dateArray == null){
+        return false;
+    }
+
+    month = dateArray[1];
+    day= dateArray[3];
+    year = dateArray[5];        
+
+    if (month < 1 || month > 12){
+        return false;
+    }else if (day < 1 || day> 31){ 
+        return false;
+    }else if ((month==4 || month==6 || month==9 || month==11) && day ==31){
+        return false;
+    }else if (month == 2){
+        var isLeapYear = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+        if (day> 29 || (day ==29 && !isLeapYear)){
+            return false
+        }
+    }
+    return true;
+}
+
+
+var finishedButton = document.getElementById('finished')
+var email = document.getElementById("email");
+var ZIP_code = document.getElementById("ZIP-code");
+var DOB = document.getElementById("DOB");
+finishedButton.onclick = function() {
+    if (checkEmpty()) {
+        return;
+    } else {
+        if (!validateDate(DOB.value)) {
+            document.getElementById("errorOfDOB").innerHTML = "You have entered a invalid date!"
+        } else {
+            document.getElementById("errorOfDOB").innerHTML = "";
+        }
+        validateEmail(email);
+        validateZipCode(ZIP_code);
+        validateSex();
+    }
+}
+
+var clearButton = document.getElementById('clear');
+clearButton.onclick = function() {
+    var allElements = document.getElementsByTagName("*");
+    for(var i = 0; i < allElements.length; i++){
+        allElements[i].value = "";
+    }
+
+    var error = document.getElementsByClassName("error");
+    for(var i = 0; i < error.length; i++){
+        error[i].innerHTML = "";
+    }
+
+    var radio = document.getElementsByClassName("radio");
+    for(var i = 0; i < radio.length; i++) {
+        radio[i].checked = false;
+    }
+}
